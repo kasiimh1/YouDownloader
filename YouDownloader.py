bab41b3daa2f1a596645ba79766c8f5616f2747c
Kasim Hussain <kasiim@Kasims-MacBook-Pro.local>
2019-08-24 04:39:49 +0100

    Rewrote some of the code to work better from the command line and accept args

--- YouDownloader.py
+++ YouDownloader.py
@@ -1,51 +1,68 @@
 from __future__ import unicode_literals
 import youtube_dl
+import sys
 
-#need to install ffmpeg for this to work
+#**** need to install ffmpeg for this to work ****#
 
-print(" ************************************************\n",
-      "Welcome to YouDownloader\n",
-      "Created By Kasiimh1\n\n",
-      "0 = Exit Program\n",
-      "1 = Download Audio Only\n",
-      "2 = Download Video in Highest Quality Available\n",
-      "************************************************")
+links = []
+ytoptions = []
 
-print("Enter Option?... ")
-option = input()
+def printHelp():
+    print("\nWelcome to YouDownloader\nCreated By Kasiimh1\n\n",
+            "-e, -exit    Exit Program\n",
+            "-h, -help    Show Help Screen\n",
+            "-l, -link    Video Links You Wish To Download In This Format -> link,link,link\n",
+            "-a, -audio   Download Audio Only\n",
+            "-v, -video   Download Video in Highest Quality Available\n")
+    return
 
-if (option == '0') is True:
-    print("Exiting Program!")
+def downloader(link, ytoptions):
+    for index, x in enumerate(link):
+        with youtube_dl.YoutubeDL(ytoptions) as ydl:
+            ydl.download([x])
+            print("DOWNLOAD FINISHED!")
+            print("Successfully Downloaded", index + 1 , "/", len(link) , "items")
+    return
+
+def exit():
+    sys.exit()
+    return
+
+if (len(sys.argv) == 1):
+    printHelp()
     exit()
 
-else:
-    print("Starting download....")
-    print("\nEnter links seperate by a comma e.g. link,link,link")
-    links = input().split(',')
+if len(sys.argv) >= 2:
     
-    if (option == '1') is True:
-        
-        ydl_opts = {'quiet': 'opts.quiet',
-            'outtmpl': '%(title)s.%(ext)s',
-            'no_warnings': 'opts.no_warnings',
-                'format': 'bestaudio/best',
-                    'postprocessors': [{
-                                       'key': 'FFmpegExtractAudio',
-                                       'preferredcodec': 'mp3',
-                                       'preferredquality': '320',
-                                       }]}
+    #exit program
+    if (sys.argv[1] == '-e' or sys.argv[1] == '-exit'):
+        print("Exiting Program!")
+        exit()
 
-if (option == '2') is True:
-    
-    ydl_opts = {'quiet': 'opts.quiet',
-        'no_warnings': 'opts.no_warnings',
-            'format': 'bestvideo+bestaudio/best',
-                'outtmpl': '%(title)s',
-                    'merge_output_format': 'mkv'
-                }
+    if (sys.argv[1] == '-help' or sys.argv[1] == '-h'):
+        printHelp()
+        exit()
 
-for index, x in enumerate(links):
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([x])
-        print("DOWNLOAD FINISHED!")
-        print("Successfully Downloaded", index + 1 , "/", len(links) , "items")        
+    if (sys.argv[1] == '-a' or sys.argv[1] == '-audio'):
+        ytoptions = {'quiet': 'opts.quiet',
+                        'no_warnings': 'opts.no_warnings',
+                        'format': 'bestaudio/best',
+                        'postprocessors': [{'key': 'FFmpegExtractAudio',
+                                            'preferredcodec': 'mp3',
+                                            'preferredquality': '320'}]}
+
+    if (sys.argv[1] == '-v' or sys.argv[1] == '-video'):
+        ytoptions = {'quiet': 'opts.quiet',
+                        'no_warnings': 'opts.no_warnings',
+                        'format': 'bestvideo+bestaudio/best',
+                        'outtmpl': '%(title)s',
+                        'merge_output_format': 'mkv'}
+
+    if (sys.argv[1] == '-v' or sys.argv[1] == '-video' and sys.argv[2] == '-l' or sys.argv[2] == '-link'):
+        links = sys.argv[3].split(',')
+        downloader(links, ytoptions)
+
+    if (sys.argv[1] == '-a' or sys.argv[1] == '-audio' and sys.argv[2] == '-l' or sys.argv[2] == '-link'):
+        links = sys.argv[3].split(',')
+        downloader(links, ytoptions)
+        exit()
